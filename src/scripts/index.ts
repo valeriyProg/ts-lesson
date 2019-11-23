841c1a92eaaa91f12e5432bf3a625e454388bec7
valeriyProg <58046061+valeriyProg@users.noreply.github.com>
2019-11-23 11:04:43 +0200

    Add table

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -9,13 +9,15 @@ const chocolateBar = new ChocolateBar(
   0.2
 );
 
-document.getElementById("app").innerHTML = `
-<h1>Hello World!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.</br>
-  ${chocolateBar}
+const productTable = document.getElementById("product-table");
 
-</div>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello World!</h1>
+// <div>
+//   Look
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
+//   for more info about Parcel.</br>
+//   ${chocolateBar}
+
+// </div>
+// `;
